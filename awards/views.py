7c1b0404f44f77f68dc6ae052588bcf357cf8761
AbugaAroni <abugaaroni@gmail.com>
2020-10-26 21:31:35 +0300

    add working homepage

--- awards/views.py
+++ awards/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    allimages = "x"
 
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html', {"images":allimages})
