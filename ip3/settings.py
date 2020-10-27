49e177441b517c74f3c898dacf9c758de097c258
AbugaAroni <abugaaroni@gmail.com>
2020-10-27 20:50:29 +0300

    will add local database information

--- ip3/settings.py
+++ ip3/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = 'ip3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'awards',
+        'USER': 'abuga',
+        'PASSWORD':'password',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
