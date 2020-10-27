4e84cead8f109a69f5f5a6e9a62b341afb1b1c86
AbugaAroni <abugaaroni@gmail.com>
2020-10-27 15:15:54 +0300

    add registration

--- ip3/urls.py
+++ ip3/urls.py
@@ -20,4 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('awards.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),    
 ]
