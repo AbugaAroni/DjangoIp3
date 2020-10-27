bc3db865bd4693064dfc6c35acf4beab6544607c
AbugaAroni <abugaaroni@gmail.com>
2020-10-27 15:56:55 +0300

    add styling tot eh homepage

--- awards/views.py
+++ awards/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Profile, Project, Rating
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    allimages = "x"
-
-    return render(request, 'homepage.html', {"images":allimages})
+    allprojects = Project.objects.all()
+    return render(request, 'homepage.html', {"projects":allprojects})
